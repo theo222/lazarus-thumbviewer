7884fdd548936f9fcd2cbdae67d485c05179f107
Andreas Frieß <a.friess@noll-it.at>
2019-03-28 14:28:41 +0100

    Fixes for fpc 3.2 fixes and Lazarus 2.0

--- fpreadjpegthumb.pas
+++ fpreadjpegthumb.pas
@@ -469,7 +469,7 @@ initialization
   for i:=0 to Om.Count-1 do
     begin
       tn:=Om.TypeNames[i];
-      ext:=Om.Extentions[tn];
+      ext:=Om.Extensions[tn];
       ir:=Om.ImageReader[tn];
       iw:=Om.ImageWriter[tn];
       if tn<>'JPEG Graphics'then if ir<>nil then ImageHandlers.RegisterImageReader(tn,ext,ir);
